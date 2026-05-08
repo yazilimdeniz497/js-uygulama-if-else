6bca871de14838bc6fcbc85d905392572dc532bc
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-08 14:31:12 +0300

    2 sorunun cevabı eklendi

---
+++ script.js
@@ -0,0 +1,25 @@
+//1) let sayi=60
+// if(sayi>10 && sayi<50){
+//     console.log("sayi 10 ile 50 arasında");
+// }else{
+//     console.log("sayi 10 ile 50 arasında değil");
+// }
+// if(sayi<0){
+//     console.log("sayi negatif");
+// }else if((sayi>0)&&(sayi%2==0)){
+//     console.log("sayi pozitif ve çift");
+
+// }else if(sayi==0){
+//     console.log("sayı 0 'dir");
+// }else {
+//     console.log("sayi pozitif tektir");
+// }
+//2)let x=20,y=30,z=40
+// let sonuc;
+// sonuc=(y>x);
+// sonuc=(x==y);
+// sonuc=(y!=z);
+// sonuc=(z>x);
+// console.log(sonuc);
+
+
